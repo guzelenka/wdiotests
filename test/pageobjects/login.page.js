aa7b0575f9e2b8d44e6220651670af226fcda40f
guzel <guzel.ozgul@aol.com>
2021-03-28 17:34:10 -0400

    first commit

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,32 +1,61 @@
 import Page from './page';
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    get inputUsername () { return $('#username') }
-    get inputPassword () { return $('#password') }
-    get btnSubmit () { return $('button[type="submit"]') }
 
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
-    login (username, password) {
-        this.inputUsername.setValue(username);
+    get inputUsername () { return $('#normal_login_email'); }
+    get inputPassword () { return $('#normal_login_password'); }
+    get buttonSubmit () { return $('.login-form-button'); }
+
+
+    get loginValidationError () { return $('//div[contains(@class, "ant-form-item-with-help")][.//input[@id="normal_login_email"]]//div[@role="alert"]'); }
+    get passwordValidatorError () { return $('//div[contains(@class, "ant-form-item-with-help")][.//input[@id="normal_login_password"]]//div[@role="alert"]'); }
+    get errorToast() {return $('.ant-notification-notice-message'); }
+
+    open (){
+        return super.open('/');
+    }
+
+
+    setLogin (email){
+        this.inputUsername.setValue(email);
+    }
+
+    setPassword (password){
         this.inputPassword.setValue(password);
-        this.btnSubmit.click(); 
     }
 
-    /**
-     * overwrite specifc options to adapt it to page object
-     */
-    open () {
-        return super.open('login');
+    clickSubmitButton (){
+        this.buttonSubmit.click();
     }
+
+    submitButtonIsDisabled() {
+        expect(this.buttonSubmit).toBeDisabled();
+    }
+
+    emptyLoginInput() {
+        this.inputUsername.clearValue();
+    }
+
+    loginRequiredError() {
+        expect(this.loginValidationError).toBeDisplayed();
+        expect(this.loginValidationError.getText()).toEqual('Required');
+    }
+
+    emptyPasswordInput() {
+        this.inputPassword.clearValue();
+    }
+
+    passwordRequiredError() {
+        expect(this.loginValidationError).toBeDisplayed();
+        expect(this.passwordValidatorError.getText()).toEqual('Required');
+    }
+
+    errorToastAppeared() {
+        expect(this.errorToast).toBeDisplayed();
+
+    }
+
 }
 
-export default new LoginPage();
+    export default new LoginPage();
+
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,15 +1,57 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
+import ProfilePage from '../pageobjects/profile';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', () => {
+
+describe('Auth', () => {
+    beforeEach(() => {
         LoginPage.open();
-
-        LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
     });
+
+    afterEach(() => {
+        browser.execute('window.localStorage.clear()');
+    });
+
+    it('Test 1: User logs in with valid data', () => {
+        LoginPage.setLogin('cegef16172@heroulo.com');
+        LoginPage.setPassword('test1234');
+        LoginPage.clickSubmitButton();
+        ProfilePage.isOpen();
+
+
+    });
+
+    it('Test 2: Login button is disabled without entering values', () => {
+        LoginPage.submitButtonIsDisabled();
+
+    });
+
+
+
+    it('Test 3: Login input is required', () => {
+        LoginPage.setLogin('example@mail.com')
+        LoginPage.emptyLoginInput();
+        LoginPage.loginRequiredError();
+    });
+
+
+    it('Test 4: Password input is required', () => {
+        LoginPage.setLogin('example@mail.com')
+        LoginPage.setPassword('123')
+        LoginPage.emptyPasswordInput();
+        LoginPage.passwordRequiredError();
+    });
+
+
+     it('Test 5: Fails with invalid data', () => {
+        LoginPage.setLogin('example@example.com');
+        LoginPage.setPassword('123456');
+        LoginPage.clickSubmitButton();
+        LoginPage.errorToastAppeared();
+
+    });
+
+
+
 });
 
 
