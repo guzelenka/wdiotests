a571c7b39c298252d4d0687decf4f7570e3d0852
guzel <guzel.ozgul@aol.com>
2021-05-09 20:20:15 -0400

    circle

--- firefox.headless.conf.js
+++ firefox.headless.conf.js
@@ -1,18 +1,13 @@
+const hooks = require('./config/hooks.conf');
+const base = require('./config/base.conf');
+
+
 exports.config = {
 
     runner: 'local',
 
-    specs: [
-        './test/specs/**/*.js'
-    ],
-
-    exclude: [
-
-    ],
-
     maxInstances: 1,
     capabilities: [
-
         {
             maxInstances: 1,
             browserName: 'firefox',
@@ -22,29 +17,10 @@ exports.config = {
         }
     ],
 
-    logLevel: 'info',
-
-    bail: 0,
-
-    baseUrl: 'https://localcoding.us',
-
-    waitforTimeout: 10000,
-
-    connectionRetryTimeout: 120000,
-
-    connectionRetryCount: 3,
-
     services: ['geckodriver'],
 
-    framework: 'mocha',
 
-    reporters: ['spec'],
-
-    mochaOpts: {
-        // Babel setup
-        require: ['@babel/register'],
-        ui: 'bdd',
-        timeout: 60000
-    },
+    ...base,
+    ...hooks
 
    }
